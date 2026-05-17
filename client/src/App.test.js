0956932831296c22288541f9a6aff055888c5eb0
Rahul K <rahulkulal082@gmail.com>
2026-05-17 11:18:13 +0530

    Delete client/src/App.test.js

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the login screen initially', () => {
-  render(<App />);
-  const welcomeText = screen.getByText(/Welcome Back/i);
-  expect(welcomeText).toBeInTheDocument();
-});
