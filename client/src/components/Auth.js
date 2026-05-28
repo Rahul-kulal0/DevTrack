e7962a4840dfada4050514fbf458643acc0ae6ba
Rahul Kulal <rahulkulal082@gmail.com>
2026-05-28 12:07:31 +0530

    Fixed AWS backend API URL

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -23,7 +23,7 @@ const Auth = ({ onAuthSuccess }) => {
       : formData;
 
     try {
-      const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+      const API_URL = process.env.REACT_APP_API_URL || 'http://16.16.182.156:5000';
 
       const res = await fetch(`${API_URL}${endpoint}`, {
         method: 'POST',
